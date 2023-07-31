b1dee3c0bc1d87fd23b93690560726fe24cdabfd
dkswndrnjs12 <dkswndrnjs12@gmail.com>
2023-07-31 19:15:10 +0900

    line color

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -1,3 +1,6 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
@@ -39,6 +42,13 @@ function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
 }
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color
+  ctx.strokeStyle= colorValue;
+  ctx.fillStyle= colorValue;
+  color.value = colorValue;
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -47,40 +57,5 @@ canvas.addEventListener("mouseleave", cancelPainting);
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
-// 선 그리기
-// function mousemove(event) {
-//   ctx.beginPath();
-//   ctx.moveTo(0,0);
-//   const color = colors[Math.floor(Math.random() * colors.length)];
-//   ctx.strokeStyle = color;
-//   ctx.lineTo(event.offsetX, event.offsetY);
-//   ctx.stroke();
-// }
-// canvas.addEventListener("mousemove", mousemove);
 
-// 집 만들기
-// ctx.fillRect(200, 200, 50, 200);
-// ctx.fillRect(400, 200, 50, 200);
-// ctx.lineWidth = 2;
-// ctx.fillRect(300, 300, 50, 100);
-// ctx.fillRect(200, 200, 200, 20);
-// ctx.moveTo(200, 200);
-// ctx.lineTo(325, 100);
-// ctx.lineTo(450, 200);
-// ctx.fill();
-
-// 사람만들기
-// ctx.fillRect(210 - 40, 200, 15, 100);
-// ctx.fillRect(350 - 40, 200, 15, 100);
-// ctx.fillRect(260 - 40, 200, 60, 200);
-
-// ctx.arc(250, 150, 50, 0, 2*Math.PI);
-// ctx.fill();
-
-// ctx.beginPath();
-// ctx.fillStyle = "white";
-// ctx.arc(230, 130, 8, Math.PI, 2 * Math.PI);
-// ctx.arc(270, 130, 8, Math.PI, 2 * Math.PI);
-// ctx.fill();
-
-
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
