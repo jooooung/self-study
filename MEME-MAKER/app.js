eb405c8e4e724ae9c816902e017b90d621accaad
dkswndrnjs12 <dkswndrnjs12@gmail.com>
2023-08-01 15:09:13 +0900

    지우기

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -1,10 +1,15 @@
-const colorOptions = Array.from(
+const modeBtn = document.getElementById("mode-btn");  // 그리기모드
+const destroyBtn = document.getElementById("destroy-btn");  // 전체지우기
+const eraserBtn = document.getElementById("eraser-btn");  // 지우개
+const colorOptions = Array.from(  // 색 옵션 배열
   document.getElementsByClassName("color-option")
 );
-const color = document.getElementById("color");
+const color = document.getElementById("color"); 
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 const colors = [
   "#1abc9c",
   "#2ecc71",
@@ -14,10 +19,12 @@ const colors = [
   "#bdc3c7"
 ]
 
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -49,8 +56,37 @@ function onColorClick(event) {
   ctx.fillStyle= colorValue;
   color.value = colorValue;
 }
+
+function onModeClick(event) {
+  if(isFilling){
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  }else{
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if(isFilling){
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  modeBtn.innerText = "Fill";
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("click", onCanvasClick);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
@@ -59,3 +95,7 @@ color.addEventListener("change", onColorChange);
 
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
