1952a6e73956c1ee7090a5c2e0464a5694c27793
dkswndrnjs12 <dkswndrnjs12@gmail.com>
2023-08-21 15:53:43 +0900

    WebSocket Server

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -5,8 +7,14 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000);
+
+// http 서버
+const server = http.createServer(app);
+// webSocket 서버
+const wss = new WebSocketServer({ server });
+
+server.listen(3000, handleListen);
