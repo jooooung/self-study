47a0b4a4be7bb70a1f90a7e9ce9fd955a28a01b4
dkswndrnjs12 <dkswndrnjs12@gmail.com>
2023-08-21 21:39:10 +0900

    WebSocket Messages

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -17,10 +17,13 @@ const server = http.createServer(app);
 // webSocket 서버
 const wss = new WebSocketServer({ server });
 
-function handleConnection(socket){
-  console.log(socket);
-}
-
-wss.on("connection", handleConnection)
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", message => {
+    console.log(message.toString());
+  });
+  socket.send("hello!!");
+});
 
 server.listen(3000, handleListen);
