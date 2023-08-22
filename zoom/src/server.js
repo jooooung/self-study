750f34376bd0a45f402375aac22d868b28e00dcf
dkswndrnjs12 <dkswndrnjs12@gmail.com>
2023-08-22 21:01:52 +0900

    chat

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -25,10 +25,21 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket); // 브라우저 간 소통
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser ✅");
   socket.on("close", onSocketClose);
-  socket.on("message", (message) => {
-    sockets.forEach(aSocket => aSocket.send(message.toString('utf8')));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch(message.type){
+      case "new message":
+        sockets.forEach(aSocket => 
+          aSocket.send(`${socket.nickname} : ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
 });
 
