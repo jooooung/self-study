1c1c9117f4f0dd3f62ce7d3ee134e0c6b0da55a6
dkswndrnjs12 <dkswndrnjs12@gmail.com>
2023-08-29 17:10:36 +0900

    Admin panel

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
-import socketIO from "socket.io";
+import { Server } from "socket.io";
 import express from "express";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -11,7 +12,17 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = socketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+  mode: "development",
+});
 
 function publicRooms() {
   const {
