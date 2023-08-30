22380b1234294b5c2774f19486995beea0b0829a
dkswndrnjs12 <dkswndrnjs12@gmail.com>
2023-08-30 17:12:01 +0900

    connect camera

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,81 +1,127 @@
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+// // text chat
+// const welcome = document.getElementById("welcome");
+// const form = welcome.querySelector("form");
+// const room = document.getElementById("room");
 
-room.hidden = true;
+// room.hidden = true;
 
-let roomName;
+// let roomName;
 
-function showRoom() {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}`;
-  const msgForm = room.querySelector("#msg");
-  const nameForm = room.querySelector("#name");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-  nameForm.addEventListener("submit", handleNicknameSubmit);
-} // 채팅방 보이게
+// function showRoom() {
+//   welcome.hidden = true;
+//   room.hidden = false;
+//   const h3 = room.querySelector("h3");
+//   h3.innerText = `Room ${roomName}`;
+//   const msgForm = room.querySelector("#msg");
+//   const nameForm = room.querySelector("#name");
+//   msgForm.addEventListener("submit", handleMessageSubmit);
+//   nameForm.addEventListener("submit", handleNicknameSubmit);
+// } // 채팅방 보이게
 
-function addMessage(message) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.appendChild(li);
-} // 입장 메세지 추가
+// function addMessage(message) {
+//   const ul = room.querySelector("ul");
+//   const li = document.createElement("li");
+//   li.innerText = message;
+//   ul.appendChild(li);
+// } // 입장 메세지 추가
 
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
-} // 채팅방 입장
+// function handleRoomSubmit(event) {
+//   event.preventDefault();
+//   const input = form.querySelector("input");
+//   socket.emit("enter_room", input.value, showRoom);
+//   roomName = input.value;
+//   input.value = "";
+// } // 채팅방 입장
 
-function handleNicknameSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("#name input");
-  socket.emit("nickname", input.value);
-} // 닉네임 입력
+// function handleNicknameSubmit(event) {
+//   event.preventDefault();
+//   const input = room.querySelector("#name input");
+//   socket.emit("nickname", input.value);
+// } // 닉네임 입력
 
-function handleMessageSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-  socket.emit("new message", input.value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
-  input.value = "";
-} // 메세지 입력
+// function handleMessageSubmit(event) {
+//   event.preventDefault();
+//   const input = room.querySelector("#msg input");
+//   const value = input.value;
+//   socket.emit("new message", input.value, roomName, () => {
+//     addMessage(`You: ${value}`);
+//   });
+//   input.value = "";
+// } // 메세지 입력
 
-form.addEventListener("submit", handleRoomSubmit);
+// form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", (user, newCount) => { 
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`; 
-  addMessage(`${user} joined!`);
-}); // 채팅방 입장 메세지
+// socket.on("welcome", (user, newCount) => {
+//   const h3 = room.querySelector("h3");
+//   h3.innerText = `Room ${roomName} (${newCount})`;
+//   addMessage(`${user} joined!`);
+// }); // 채팅방 입장 메세지
 
-socket.on("bye", (left, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`; 
-  addMessage(`${left} left`);
-}); // 채팅방 퇴장 메세지
+// socket.on("bye", (left, newCount) => {
+//   const h3 = room.querySelector("h3");
+//   h3.innerText = `Room ${roomName} (${newCount})`;
+//   addMessage(`${left} left`);
+// }); // 채팅방 퇴장 메세지
 
-socket.on("new message", addMessage); // 메세지 출력
+// socket.on("new message", addMessage); // 메세지 출력
 
-socket.on("room_change", (rooms) => {
-  const roomList = welcome.querySelector("ul");
-  roomList.innerHTML = "";
-  if(rooms.length === 0){
-    
-    return;
+// socket.on("room_change", (rooms) => {
+//   const roomList = welcome.querySelector("ul");
+//   roomList.innerHTML = "";
+//   if(rooms.length === 0){
+//     return;
+//   }
+//   rooms.forEach((room) => {
+//     const li = document.createElement("li");
+//     li.innerText = room;
+//     roomList.append(li);
+//   });
+// });
+
+// video chat
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
   }
-  rooms.forEach((room) => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.append(li);
-  });
-});
+}
+
+getMedia();
+
+function handleMuteClick() {
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  if(cameraOff){
+    cameraBtn.innerText = "Camera Off"
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Camera On"
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
--- zoom/src/server.js
+++ zoom/src/server.js
@@ -24,77 +24,55 @@ instrument(wsServer, {
   mode: "development",
 });
 
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
+const handleListen = () => console.log("Listening on http://localhost:3000");
+httpServer.listen(3000, handleListen);
 
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(`Socket Event:${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit("room_change", publicRooms());
-  }); // 채팅방 입장 메세지
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
-  }); // 채팅방 퇴장 메세지
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-  socket.on("new message", (msg, room, done) => {
-    socket.to(room).emit("new message", `${socket.nickname}: ${msg}`);
-    done();
-  }); // 메세지 출력
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname)); // 닉네임
-});
-
-function onSocketClose() {
-  console.log("Disconnected from the Browser ❌");
-}
-
-// const sockets = [];
-
-// wss.on("connection", (socket) => {
-//   sockets.push(socket); // 브라우저 간 소통
-//   socket["nickname"] = "Anon";
-//   console.log("Connected to Browser ✅");
-//   socket.on("close", onSocketClose);
-//   socket.on("message", (msg) => {
-//     const message = JSON.parse(msg);
-//     switch(message.type){
-//       case "new message":
-//         sockets.forEach(aSocket =>
-//           aSocket.send(`${socket.nickname} : ${message.payload}`)
-//         );
-//         break;
-//       case "nickname":
-//         socket["nickname"] = message.payload;
-//         break;
+// // 채팅
+// function publicRooms() {
+//   const {
+//     sockets: {
+//       adapter: { sids, rooms },
+//     },
+//   } = wsServer;
+//   const publicRooms = [];
+//   rooms.forEach((_, key) => {
+//     if (sids.get(key) === undefined) {
+//       publicRooms.push(key);
 //     }
 //   });
+//   return publicRooms;
+// }
+
+// function countRoom(roomName) {
+//   return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+// }
+
+// wsServer.on("connection", (socket) => {
+//   socket["nickname"] = "Anon";
+//   socket.onAny((event) => {
+//     console.log(`Socket Event:${event}`);
+//   });
+//   socket.on("enter_room", (roomName, done) => {
+//     socket.join(roomName);
+//     done();
+//     socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+//     wsServer.sockets.emit("room_change", publicRooms());
+//   }); // 채팅방 입장 메세지
+//   socket.on("disconnecting", () => {
+//     socket.rooms.forEach((room) =>
+//       socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+//     );
+//   }); // 채팅방 퇴장 메세지
+//   socket.on("disconnect", () => {
+//     wsServer.sockets.emit("room_change", publicRooms());
+//   });
+//   socket.on("new message", (msg, room, done) => {
+//     socket.to(room).emit("new message", `${socket.nickname}: ${msg}`);
+//     done();
+//   }); // 메세지 출력
+//   socket.on("nickname", (nickname) => (socket["nickname"] = nickname)); // 닉네임
 // });
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
-
-httpServer.listen(3000, handleListen);
+// function onSocketClose() {
+//   console.log("Disconnected from the Browser ❌");
+// }
