d0023d8f3a457d18557c5d155904e214ae086895
dkswndrnjs12 <dkswndrnjs12@gmail.com>
2023-08-31 17:12:16 +0900

    call controls

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -84,6 +84,7 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
@@ -99,11 +100,31 @@ async function getMedia() {
   } catch (e) {
     console.log(e);
   }
-}
+}// get audio, video 
 
 getMedia();
 
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices(); // 연결 기기
+    const cameras = devices.filter(device => device.kind === "videoinput"); // 연결 video 
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    })// camera select
+  } catch (e) {
+    console.log(e);
+  }
+}// camera list
+
+getCameras();
+
 function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -111,17 +132,20 @@ function handleMuteClick() {
     muteBtn.innerText = "Mute";
     muted = false;
   }
-}
+}// audio on/off
 
 function handleCameraClick() {
-  if(cameraOff){
-    cameraBtn.innerText = "Camera Off"
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Camera Off";
     cameraOff = false;
   } else {
-    cameraBtn.innerText = "Camera On"
+    cameraBtn.innerText = "Camera On";
     cameraOff = true;
   }
-}
+}// video on/off
 
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
