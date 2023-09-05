f7e5801b71678a4727630ec244aa577233108fe8
dkswndrnjs12 <dkswndrnjs12@gmail.com>
2023-09-05 14:25:00 +0900

    rooms

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -24,6 +24,14 @@ instrument(wsServer, {
   mode: "development",
 });
 
+wsServer.on("connection", socket => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+});
+
 const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
 
